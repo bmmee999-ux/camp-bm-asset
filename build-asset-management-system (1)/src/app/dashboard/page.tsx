bf6b5ee5769cfb5d9c8010c764551685a4a22b06
bmmee999-ux <bm.mee999@gmail.com>
2026-09-25 09:24:27 +0700

    Delete build-asset-management-system (1) directory

--- build-asset-management-system (1)/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { DashboardPage } from "@/components/dashboard-page";
-
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <DashboardPage />;
-}
--- build-asset-management-system (1)/src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-import { GlobalSearchPage } from "@/components/global-search-page";
-
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <GlobalSearchPage />;
-}
