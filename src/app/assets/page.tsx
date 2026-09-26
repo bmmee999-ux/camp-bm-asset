f6a78d71a12c417a546e9e3af2f2d7118f823674
bmmee999-ux <bm.mee999@gmail.com>
2026-09-26 16:13:43 +0700

    Delete src directory

--- src/app/assets/page.tsx
+++
@@ -1,7 +0,0 @@
-import { AssetsPage } from "@/components/assets-page";
-
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <AssetsPage />;
-}
--- src/app/move-asset/page.tsx
+++
@@ -1,7 +0,0 @@
-import { MoveAssetPage } from "@/components/move-asset-page";
-
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <MoveAssetPage />;
-}
